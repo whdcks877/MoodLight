92c6c8e10e99900865a43f6a98df469a71a320ba
whdcks877 <38479511+whdcks877@users.noreply.github.com>
2019-11-23 14:09:24 +0900

    Add files via upload

---
+++ MoodLight.sh
@@ -0,0 +1,1 @@
+sudo GOOGLE_APPLICATION_CREDENTIALS=/home/pi/MoodLight/service-account.json PYTHONPATH="/home/pi/MoodLight/rpi_ws281x/python/build/lib.linux-armv7l-2.7" python /home/pi/MoodLight/MoodLightWithMqtt.py
